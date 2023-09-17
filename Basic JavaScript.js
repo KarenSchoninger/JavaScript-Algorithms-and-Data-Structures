0c95bb721325d4adc4871c6dd7b8c8852acb3b17
KarenSchoninger <142519817+KarenSchoninger@users.noreply.github.com>
2023-09-17 11:20:58 +0200

    Create Basic JavaScript.js

---
+++ Basic JavaScript.js
@@ -0,0 +1,27 @@
+// Comment Your JavaScript Code:
+
+// These are the basics of JavaScript
+/* JavaScript sirve para trabajar 
+en la parte interactiva de la web.*/
+
+// Declare JavaScript Variables:
+var myName;
+
+// Storing Values with the Assignment Operator:
+var a;
+a = 7;
+
+// Assigning the Value of One Variable to Another
+var a;
+a = 7;
+var b;
+b = a;
+
+// Initializing Variables with the Assignment Operator
+var a = 9;
+
+// Declare String Variables
+var myFirstName = "Karen";
+var myLastName = "Schoninger";
+
+// Understanding Uninitialized Variables
