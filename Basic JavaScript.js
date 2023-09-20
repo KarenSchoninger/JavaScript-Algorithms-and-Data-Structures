89f2f9941a23c47fe9db88aa3dbf3b08192c85a4
KarenSchoninger <142519817+KarenSchoninger@users.noreply.github.com>
2023-09-20 18:48:03 +0200

    Update Basic JavaScript.js

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -314,17 +314,201 @@ function processArg(num) {
 processed = processArg(7);
 
 // Stand in Line
+function nextInLine(arr, item) {
+  arr.push(item);  // Add the item to the end of the array
 
+  let removedItem = arr.shift();  // Remove the first element from the array and store it in a variable
 
+  return removedItem;     // Return the element that was removed
+}
 
+          // Setup
+let testArr = [1, 2, 3, 4, 5];
 
+          // Display code
+console.log("Before: " + JSON.stringify(testArr)); // Print the original array
+console.log(nextInLine(testArr, 6)); // Call the function and print the returned item
+console.log("After: " + JSON.stringify(testArr)); // Print the modified array
 
+// Understanding Boolean Values (true/ false)
+function welcomeToBooleans() {
 
+  return true; 
 
+}
 
+// Use Conditional Logic with If Statements
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
 
+}       // if the function is met, it returns "Yes, that was true".
 
+// Comparison with the Equality Operator
+function testEqual(val) {
+  if (val == 12) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
 
+testEqual(10);
+
+// Comparison with the Strict Equality Operator  (The difference between == and === is that with === if two data of different types are compared, Java does not convert them.
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+// Practice comparing different values
+function compareEquality(a, b) {
+  if (a === b) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+// Comparison with the Inequality Operator
+function testNotEqual(val) {
+  if (val != 99) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+// Comparison with the Strict Inequality Operator
+function testStrictNotEqual(val) {
+  if (val !== 17) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+// Comparison with the Greater Than Operator
+function testGreaterThan(val) {
+  if (val > 100) {  
+    return "Over 100";
+  }
+
+  if (val > 10) {  
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+// Comparison with the Greater Than Or Equal To Operator
+function testGreaterOrEqual(val) {
+  if (val >= 20) { 
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+// Comparison with the Less Than Operator
+function testLessThan(val) {
+  if (val < 25) {  
+    return "Under 25";
+  }
+
+  if (val < 55) {  
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+// Comparison with the Less Than Or Equal To Operator
+function testLessOrEqual(val) {
+  if (val <= 12) {  
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+// Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  return "No";
+}
+
+testLogicalAnd(10);
+
+// Comparisons with the Logical Or Operator
+function testLogicalOr(val) {
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+// Introducing Else Statements
+function testElse(val) {
+  let result = "";
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    return "5 or Smaller";
+  }
+
+  return result;
+}
+
+testElse(4);
+
+// Introducing Else If Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+}
+}
+
+testElseIf(7);
+
+// Logical Order in If Else Statements
 
 
 
