a8528543bcc3840f14222e0bd925f8415a39e718
KarenSchoninger <142519817+KarenSchoninger@users.noreply.github.com>
2023-09-26 13:38:39 +0200

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -1,1 +1,54 @@
 // Compare Scopes of the var and let Keywords
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+// Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+}
+editInPlace();
+
+// Prevent Object Mutation  (Object.freeze to prevent mutation)
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  Object.freeze(MATH_CONSTANTS);
+
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+// Use Arrow Functions to Write Concise Anonymous Functions (we do not reuse them anywhere else)
+var magic = function() {
+  return new Date();
+};
+   // rewrite:
+const magic = () => new Date();
+
+// Write Arrow Functions with Parameters
+
+
+
+
+
+
+
+
+
+
