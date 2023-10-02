c139a36d1d9f4ce5e19ba7debae84cea93e937f6
KarenSchoninger <142519817+KarenSchoninger@users.noreply.github.com>
2023-10-02 16:02:30 +0200

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -110,6 +110,39 @@ const sourceWithoutFirstTwo = removeFirstTwo(source);
 console.log(sourceWithoutFirstTwo); // Resultado: [3, 4, 5, 6, 7, 8, 9, 10]
 
 // Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = ({ max, min }) => (max + min) / 2.0; 
+
+// Create Strings using Template Literals
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+
+function makeList(arr) {
+  const failureItems = [];
+
+  for (const item of arr) {
+    failureItems.push(`<li class="text-warning">${item}</li>`);
+  }
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+
+console.log(failuresList);
+
+// Write Concise Object Literal Declarations Using Object Property Shorthand
 
 
 
